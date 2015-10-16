c940959b336df9e8b0075ca58330305097a06566
朱明明 <2428922347@qq.com>
2015-10-16 18:19:24 +0800

    完善

--- Horsemen/src/main/resources/public/app/indexController.js
+++ Horsemen/src/main/resources/public/app/indexController.js
@@ -1,6 +1,14 @@
 myAppModule.controller('indexController', ["$scope", "Ajax", function ($scope, Ajax) {
     $scope.name = 'xingoo';
-    $scope.show = function () {
-        Ajax.alert1();
+
+    var menu = [[{name: '录入', url: "/addView"}, {name: '检索', url: "/searchView"}], [{name: '举报', url: "/reportView"}]]
+    $scope.menuDate = menu[0];
+
+    $scope.menu = function (index) {
+        $scope.menuDate = menu[index || 0];
+    }
+    $scope.active = function (index) {
+        $(index).parent().siblings().removeClass("active")
+        $(index).parent().addClass("active")
     }
 }]);
--- Horsemen/src/main/resources/public/app/indexViewController.js
+++ Horsemen/src/main/resources/public/app/indexViewController.js
@@ -3,4 +3,9 @@
  */
 myAppModule.controller('indexViewController', ["$scope", function ($scope) {
     $scope.name = 'index view';
+    var menu = [[{name: '录入', url: "/addView"}, {name: '检索', url: "/searchView"}], [{name: '举报', url: "/reportView"}]]
+    $scope.menuDate = [];
+    $scope.menu = function (index) {
+        $scope.menuDate = menu[index || 0];
+    }
 }]);
